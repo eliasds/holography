10108afbb5118472211b6af4af771e49a096f872
eliasds <eliasds@gmail.com>
2014-07-03 12:35:53 -0700

    Added coded aperture mask option
    Added an option to input a coded aperture mask function and reformatted
    code a little

--- propagate.m
+++ propagate.m
@@ -5,29 +5,32 @@
 % This function produces the digital refocusing of a hologram.
 % (ref pg 67,J Goodman, Introduction to Fourier Optics)
 % Version 5 works on CUDA enabled GPU if available.
-% function [Eout,Hout] = propagate(Ein,lambda,Z,ps,zpad)
+% function [Eout,H] = propagate(Ein,lambda,Z,ps,zpad)
 % inputs:
-%         Ein - complex field at input plane
+%         Ein    - complex field at input plane
 %         lambda - wavelength of light [m]
-%         Z - vector of propagation distances [m], (can be negative)
-%               i.e. "linspace(0,10E-3,500)" or "0.01"
-%         ps - pixel size [m]
+%         Z      - vector of propagation distances [m], (can be negative)
+%                  i.e. "linspace(0,10E-3,500)" or "0.01"
+%         ps     - pixel size [m]
 % optional inputs:
 %         'zpad' - size of propagation kernel desired in pixels
-%               i.e.  'zpad',1024  or 'zpad',2048
-%         'cpu' - forces use of CPU, even if a CUDA GPU is available
+%                  i.e.  'zpad',1024  or 'zpad',2048
+%         'cpu'  - forces use of CPU, even if a CUDA GPU is available
+%         'mask' - Allows for using a coded aperture mask. Mask image must
+%                  be same size as padded image, i.e. 'mask',mask
 % outputs:
-%         Eout - propagated complex field
-%         Hout - propagation kernel to check for aliasing
+%         Eout   - propagated complex field
+%         H      - propagation kernel to check for aliasing
 %
 % Daniel Shuldman, UC Berkeley, eliasds@gmail.com
 
 
-function [Eout,Hout] = propagate(Ein,lambda,Z,ps,varargin)
+function [Eout,H] = propagate(Ein,lambda,Z,ps,varargin)
 
 % Set Defaults and detect GPU and initial image size
 [m,n]=size(Ein);  M=m;  N=n;
 gpu_num = gpuDeviceCount; %Determines if there is a CUDA enabled GPU
+mask=1; %Default Aperture Mask function
 
 if nargin==3
     ps=6.5e-6;
@@ -54,17 +57,21 @@ end
         
 while ~isempty(varargin)
     switch upper(varargin{1})
-         
+        
         case 'ZPAD'
             zpad = varargin{2};
             varargin(1:2) = [];
             M=zpad;
             N=zpad;
-             
+            
         case 'CPU'
             varargin(1) = [];
             gpu_num = 0;
-             
+            
+        case 'MASK'
+            mask = varargin{2};
+            varargin(1:2) = [];
+            
         otherwise
             error(['Unexpected option: ' varargin{1}])
     end
@@ -76,7 +83,7 @@ if gpu_num == 0;
     Eout = zeros(m,n,length(Z));
     aveborder=mean(cat(2,Ein(1,:),Ein(m,:),Ein(:,1)',Ein(:,n)'));
     if nargout>1
-        Hout = zeros(M,N,length(Z));
+        H = zeros(M,N,length(Z));
     end
 else
     % reset(gpuDevice(1));
@@ -86,7 +93,7 @@ else
     Eout = gpuArray.zeros(m,n,length(Z));
     aveborder=gpuArray(mean(cat(2,Ein(1,:),Ein(m,:),Ein(:,1)',Ein(:,n)')));
     if nargout>1
-        Hout = gpuArray.zeros(M,N,length(Z));
+        H = gpuArray.zeros(M,N,length(Z));
     end
 end
 %k=(2*pi/lambda);  %wavenumber
@@ -110,7 +117,7 @@ for z = 1:length(Z)
     %h(:,:,z) = exp(1i*k*z)*exp(1i*k*(x.^2+y.^2)/(2*z))/(1i*lambda*z); %Fresnel kernel
     %H  = exp(1i*k*Z(z))*exp(-1i*pi*lambda*Z(z)*(fx2fy2)); %Correct Transfer Function
     H  = exp(-1i*pi*lambda*Z(z)*fx2fy2); %Fast Transfer Function
-    Eout_pad=ifft2(ifftshift(E0fft.*H));
+    Eout_pad=ifft2(ifftshift(E0fft.*H.*mask));
 
     Eout(:,:,z)=Eout_pad(1+(M-m)/2:(M+m)/2,1+(N-n)/2:(N+n)/2);
 end
@@ -120,6 +127,6 @@ end
 if gpu_num > 0;
     Eout=gather(Eout);
     if nargout > 1
-        Hout=gather(Hout);
+        H=gather(H);
     end
 end
