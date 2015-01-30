c7847fcadd93cb7118db49cc71d74ff64d6bf20b
eliasds <eliasds@gmail.com>
2015-01-30 14:56:19 -0800

    updating threshold detection. still needs work

--- detect.m
+++ detect.m
@@ -147,36 +147,24 @@ end
 %
 % Holo_0001 = (double(imread([filesort(L).name]))./background);
 % [Imin_0001, ~] = imin(Holo_0001,lambda/refractindex,Z,ps/mag,zpad);
-% Imin_0001 = imcrop(Imin,rect);
-nbins = round(0.5*sqrt(numel(Imin)));
-[bincount,edges] = histcounts(Imin(:),nbins);
-bincount = bincount(1:round(end/2));
-edges = edges(1:round(end/2));
-[M I] = min(bincount(1:round(end/2)));
-figure(6);histogram(Imin(:),nbins)
-nbins=100;
-figure(7);plot(bincount)
-axis([0,nbins,0,5000]);
-figure(8);bincount = smooth(bincount);plot(bincount)
-[M I] = min(bincount(1:round(end/2)))
-th_new = edges(round(I/2))
-axis([0,nbins,0,5000])
-figure(9);bincount = smooth(bincount,'sgolay',2);plot(bincount)
-[M I] = min(bincount(1:round(end/2)))
-th_new = edges(round(I/2))
-axis([0,nbins,0,5000]);
-figure(10);bincount = smooth(bincount,'sgolay',4);plot(bincount)
-[M I] = min(bincount(1:round(end/2)))
-th_new = edges(round(I/2))
-axis([0,nbins,0,5000]);
-figure(11);bincount = smooth(bincount,'lowess');plot(bincount)
-[M I] = min(bincount(1:round(end/2)))
-th_new = edges(round(I/2))
-axis([0,nbins,0,5000]);
-figure(12);bincount = smooth(bincount,'loess');plot(bincount)
-[M I] = min(bincount(1:round(end/2)))
-th_new = edges(round(I/2))
-axis([0,nbins,0,5000]);
+Imin0001 = imcrop(Imin,rect);
+nbins = round(sqrt(numel(Imin0001)));
+[bincount,edges] = histcounts(Imin0001(:),nbins);
+figure(6);histogram(Imin0001(:),nbins)
+nbins=500;
+bincount = bincount(1:nbins);
+edges = edges(1:nbins);
+[M I] = min(bincount(1:nbins))
+figure(7);plot(edges,bincount)
+I2 = round(I/2);
+th_new = edges(I2)
+axis([0,max(edges),0,5000]);
+
+figure(10);bincount6 = smooth(bincount,'lowess');plot(edges,bincount6)
+[M I] = min(bincount6(1:nbins))
+I2 = round(I/10);
+th_new = edges(I2)
+axis([0,max(edges),0,5000]);
 
 
 %% Create Imin MAT files and run Particle Detection together
