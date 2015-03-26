c7097159ac4edb9408555b89d8c542f447c47525
eliasds <eliasds@gmail.com>
2015-03-26 11:47:01 -0700

    Update in findparticles.m

--- deconvolution/findparticles.m
+++ deconvolution/findparticles.m
@@ -1,41 +1,56 @@
 %% Find Deconvolved Particle Locations
+J2 = J{2};
 J2pos = Jpos{2};
 J2real = Jreal{2};
 
+figure(97);
+hist(J2(:),1000);title('Norm');axis([0,max(J2(:)),0,100])
 figure(98);
-hist(J2pos(:),1000);title('POS');
+hist(J2pos(:),1000);title('POS');axis([0,1.6,0,100])
 figure(99);
-hist(J2real(:),1000);title('REAL');
+hist(J2real(:),1000);title('REAL');axis([0,1.6,0,100])
+
+thresh = 2.4;
+mat = J2 > thresh; [ xyzlocations ] = findxyz( mat ); figure(97); plot3(xyzlocations(:,1),xyzlocations(:,2),xyzlocations(:,3),'b.'); box; grid minor; grid on; axis([0,512,0,512,0,251]); title('Norm');
+
+for L = 1:length(xyzlocations)
+xyzlocations(L,4) = J2(xyzlocations(L,2),xyzlocations(L,1),xyzlocations(L,3));
+end
 
 
-threshPOS = 1.07;
-threshREAL = 1.15;
-
+threshPOS = 0.2;
 matPOS = J2pos > threshPOS; [ xyzlocationsPOS ] = findxyz( matPOS ); figure(98); plot3(xyzlocationsPOS(:,1),xyzlocationsPOS(:,2),xyzlocationsPOS(:,3),'b.'); box; grid minor; grid on; axis([0,512,0,512,0,251]); title('POS');
+
+threshREAL = 0.36;
 matREAL = J2real > threshREAL; [ xyzlocationsREAL ] = findxyz( matREAL ); figure(99); plot3(xyzlocationsREAL(:,1),xyzlocationsREAL(:,2),xyzlocationsREAL(:,3),'b.'); box; grid minor; grid on; axis([0,512,0,512,0,251]); title('REAL');
 
-% if exist('Holo','var')
 for L = 1:length(xyzlocationsPOS)
 xyzlocationsPOS(L,4) = J2pos(xyzlocationsPOS(L,2),xyzlocationsPOS(L,1),xyzlocationsPOS(L,3));
 end
-% end
-% if exist('Holo','var')
+
 for L = 1:length(xyzlocationsREAL)
 xyzlocationsREAL(L,4) = J2real(xyzlocationsREAL(L,2),xyzlocationsREAL(L,1),xyzlocationsREAL(L,3));
 end
-% end
+
 
 
 % x_obj = x_obj+256
 % y_obj = y_obj+256
-xyzFoundPOS = zeros(5,4)
-xyzFoundREAL = zeros(5,4)
+xyzFound = zeros(5,4);
+xyzFoundPOS = zeros(5,4);
+xyzFoundREAL = zeros(5,4);
 % Manually edit these records from correct xyzlocationsPOS and xyzlocationsREAL
 
 % xyzTrue = zeros(5,4)
 dx = 5.5e-6/4;
 dy = 5.5e-6/4;
 dz = 24E-6;
+for L = 1:5
+errorX(L) = dx*abs(xyzFound(L,1) - xyzTrue(L,1));
+errorY(L) = dy*abs(xyzFound(L,2) - xyzTrue(L,2));
+errorZ(L) = dz*abs(xyzFound(L,3) - xyzTrue(L,3));
+end
+errorTOTpos = sqrt(errorX.^2 + errorY.^2 + errorZ.^2);
 for L = 1:5
 errorXpos(L) = dx*abs(xyzFoundPOS(L,1) - xyzTrue(L,1));
 errorYpos(L) = dy*abs(xyzFoundPOS(L,2) - xyzTrue(L,2));
@@ -50,6 +65,15 @@ end
 errorTOTreal = sqrt(errorXreal.^2 + errorYreal.^2 + errorZreal.^2);
 
 
+figure(97)
+bar([errorX'*1e6,errorY'*1e6])
+title('Deviation in Deconvolved Position; X/Y NORM')
+xlabel('Particle Number')
+ylabel('Deviation in X/Y-Direction Only (um)')
+colormap jet
+legend('X','Y')
+axis([0.5,5.5,0,2])
+
 figure(98)
 bar([errorXpos'*1e6,errorYpos'*1e6,errorZpos'*1e6])
 title('Deviation in Deconvolved Position; X/Y/Z POS')
@@ -60,7 +84,7 @@ legend('X','Y','Z')
 axis([0.5,5.5,0,30])
 
 figure(99)
-bar([errorXpos'*1e6,errorYpos'*1e6,errorZpos'*1e6])
+bar([errorXreal'*1e6,errorYreal'*1e6,errorZreal'*1e6])
 title('Deviation in Deconvolved Position; X/Y/Z REAL')
 xlabel('Particle Number')
 ylabel('Deviation in X/Y/Z-Direction Only (um)')
