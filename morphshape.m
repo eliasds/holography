3ce43a0c4768a028f7b760eadd30589ec3d9991b
Daniel Shuldman <eliasds@gmail.com>
2015-06-02 17:43:01 -0700

    morphshape: reworked to only output one shape

--- morphshape.m
+++ morphshape.m
@@ -1,38 +1,31 @@
-%% Create Dilate and Erode Parameters
-function [disk1, disk0]=morphshape(dilaterode)
+%% Create Dilate and Erode Shape Parameters
+function diskshape = morphshape(dilaterode)
 
 if dilaterode <= 2
-    disk1 = logical(ones(dilaterode-1));
+    diskshape = logical(ones(dilaterode-1));
 elseif dilaterode == 3
-    disk1 = logical(ones(dilaterode));
+    diskshape = logical(ones(dilaterode));
 elseif dilaterode == 4
-    disk1 = getnhood(strel('diamond', round((dilaterode+1)/2)));
-    disk1 = disk1(2:end-1,2:end-1);
-    disk1(:,dilaterode/2) = [];
-    disk1(dilaterode/2,:) = [];
+    diskshape = getnhood(strel('diamond', round((dilaterode+1)/2)));
+    diskshape = diskshape(2:end-1,2:end-1);
+    diskshape(:,dilaterode/2) = [];
+    diskshape(dilaterode/2,:) = [];
 elseif dilaterode == 5
-    disk1 = getnhood(strel('diamond', round((dilaterode)/2)));
-    disk1 = disk1(2:end-1,2:end-1);
+    diskshape = getnhood(strel('diamond', round((dilaterode)/2)));
+    diskshape = diskshape(2:end-1,2:end-1);
 elseif dilaterode == 6
-    disk1 = getnhood(strel('diamond', round((dilaterode+1)/2)));
-    disk1 = disk1(2:end-1,2:end-1);
-    disk1(:,dilaterode/2) = [];
-    disk1(dilaterode/2,:) = [];
+    diskshape = getnhood(strel('diamond', round((dilaterode+1)/2)));
+    diskshape = diskshape(2:end-1,2:end-1);
+    diskshape(:,dilaterode/2) = [];
+    diskshape(dilaterode/2,:) = [];
 elseif dilaterode == 7
-    disk1 = getnhood(strel('diamond', round((dilaterode)/2)));
-    disk1 = disk1(2:end-1,2:end-1);
+    diskshape = getnhood(strel('diamond', round((dilaterode)/2)));
+    diskshape = diskshape(2:end-1,2:end-1);
 elseif dilaterode == 8
-    disk1 = getnhood(strel('disk', 5));
-    disk1(:,dilaterode/2) = [];
-    disk1(dilaterode/2,:) = [];
-elseif dilaterode == 9
-    [xx,yy] = ndgrid((1:dilaterode)-((dilaterode+1)/2),(1:dilaterode)-((dilaterode+1)/2));
-    disk1 = (xx.^2 + yy.^2)<((dilaterode+1)/2)^2;
-else
-    [xx,yy] = ndgrid((1:dilaterode)-((dilaterode+1)/2),(1:dilaterode)-((dilaterode+1)/2));
-    disk1 = (xx.^2 + yy.^2)<((dilaterode+1)/2)^2;
-    dilaterode=dilaterode-1;
-    [xx,yy] = ndgrid((1:dilaterode)-((dilaterode+1)/2),(1:dilaterode)-((dilaterode+1)/2));
-    disk0 = (xx.^2 + yy.^2)<((dilaterode+1)/2)^2;
-    % disk = 1 - disk;
+    diskshape = getnhood(strel('disk', 5));
+    diskshape(:,dilaterode/2) = [];
+    diskshape(dilaterode/2,:) = [];
+% elseif dilaterode > 8
+%     [xx,yy] = ndgrid((1:dilaterode)-((dilaterode+1)/2),(1:dilaterode)-((dilaterode+1)/2));
+%     diskshape = (xx.^2 + yy.^2)<((dilaterode+1)/2)^2;
 end
