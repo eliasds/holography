23d3d905cf8de9f10ea7188e2b79e122cb724b89
Daniel Shuldman <eliasds@gmail.com>
2015-06-03 16:35:04 -0700

    renamed mask.m to applymask.m

--- applymask.m
+++ applymask.m
@@ -1,4 +1,4 @@
-function Eout = mask(Ein ,aperture, zpad)
+function Eout = applymask(Ein ,mask, zpad)
 
 %{
 if length(size(Ein)) == 2
@@ -15,15 +15,15 @@ if length(size(Ein)) == 2
         xpad=zpad(2);
     end
     if nargin < 2
-        aperture = ones(ypad,xpad);
-        aperture(:,1:xpad/2-1) = 0;
+        mask = ones(ypad,xpad);
+        mask(:,1:xpad/2-1) = 0;
     end
 
     avgborder=mean(cat(2,Ein(1,:),Ein(m,:),Ein(:,1)',Ein(:,n)'));
     Ein_pad=ones(ypad,xpad)*avgborder; %pad by average border value to avoid sharp jumps
     Ein_pad(1+(ypad-m)/2:(ypad+m)/2,1+(xpad-n)/2:(xpad+n)/2)=Ein;
     EinFFT = fftshift(fft2(Ein_pad));
-    Eout=EinFFT.*aperture;
+    Eout=EinFFT.*mask;
     Eout=ifft2(ifftshift(Eout));
     Eout=Eout(1+(ypad-m)/2:(ypad+m)/2,1+(xpad-n)/2:(xpad+n)/2);
 
@@ -44,8 +44,8 @@ if length(size(Ein)) == 3
         xpad=zpad(2);
     end
     if nargin < 2
-        aperture = ones(ypad,xpad,o);
-        aperture(:,1:xpad/2-1,:) = 0;
+        mask = ones(ypad,xpad,o);
+        mask(:,1:xpad/2-1,:) = 0;
     end
 
     % avgborder=mean(cat(2,squeeze(Ein(1,:,:)),squeeze(Ein(m,:,:)),squeeze(Ein(:,1,:)'),squeeze(Ein(:,n,:)')));
@@ -53,7 +53,7 @@ if length(size(Ein)) == 3
     % Ein_pad(1+(ypad-m)/2:(ypad+m)/2,1+(xpad-n)/2:(xpad+n)/2)=Ein;
     Ein_pad = Ein;
     EinFFT = fftshift(fft2(Ein_pad));
-    Eout = EinFFT.*aperture;
+    Eout = EinFFT.*mask;
     Eout = ifft2(ifftshift(Eout));
     Eout = abs(Eout);
     % Eout = Eout(1+(ypad-m)/2:(ypad+m)/2,1+(xpad-n)/2:(xpad+n)/2);
